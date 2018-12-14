96a44b120038b1eb2868ac807c0bab34ba052f68
RichFisher <rich.fishernc@gmail.com>
2018-12-14 08:30:23 -0600

    i never knew the human eye could leak so much fluid till today

--- src/components/linkCard/linkCard.js
+++ src/components/linkCard/linkCard.js
@@ -4,7 +4,10 @@ class linkCard extends React.Component {
   render() {
     return (
       <div>
-
+        <p>{linkName}</p>
+        <a href={link}>{link}</a>
+        <button type='button' className='btn btn-sm btn-danger'>Remove</button>
+        <input type='checkbox'>Done!</input>
       </div>
     );
   }
